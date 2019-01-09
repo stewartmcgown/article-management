6f56693602e986af68a861eb455ea926d4b529f9
Stewart McGown <stewartcallummcgown@gmail.com>
2019-01-09 22:41:12 +0000

    Fixed typerror

--- src/es6/Editors.js
+++ src/es6/Editors.js
@@ -1,15 +1,16 @@
 const Editor = require("./people/Editor")
+const SheetUtils = require("./utils/SheetUtils")
 
 class Editors {
     static async getEditorByEmail(email) {
-        const data = await SheetUtils.getSheetAsJSON(AMS.baseAuthSheet)
+        const data = await SheetUtils.getSheetAsJSON("Keys")
         const rows = data.filter(e => e.email == email)
         if (rows[0]) return new Editor(rows[0])
         else return null
     }
 
     static async updateEditorByEmail(email, rowData) {
-        SheetUtils.updateMatchingRow({ email }, rowData, AMS.baseAuthSheet)
+        SheetUtils.updateMatchingRow({ email }, rowData, "Keys")
     }
 }
 
